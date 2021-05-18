c3dc47852d4987824af92d25dd2a35f85fe75355
Marcin Makurat <mm@next-solution.pl>
2021-05-18 12:44:45 +0200

    first commit

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+
+target 'UserList-app' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'ServiceFramework', :path => '../ServiceFramework/', :inhibit_warnings => false
+  pod 'CocoaLumberjack'
+
+  target 'UserList-appTests' do
+    # Pods for testing
+  end
+
+end
---
+++ UserList-app/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  UserList-app
+//
+//  Created by Marcin Makurat on 17/05/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
